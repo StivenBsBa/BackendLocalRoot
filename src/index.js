bdd3659b44d44629394ada47d79b18ff3a239263
brayans-barajasa <brayans.barajas@unac.edu.co>
2023-11-02 15:46:15 -0500

    eliminar archivos

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const app = require("./app");
-require("dotenv").config();
-
-const server = http.createServer(app);
-
-server.listen(process.env.PORT, ()=>{
-    console.log(`Server Up, Port: ${process.env.PORT}`);
-})
